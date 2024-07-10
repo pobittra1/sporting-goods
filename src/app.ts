f457b98c3828b0b3ea5063facd5b9409daed446e
pobittra1 <pobittrachandraroy2001@gmail.com>
2024-07-10 15:36:54 +0600

    succesfully backend server and fix issue with install tsnd . also create .env file and set url from mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+
 const app: Application = express();
-const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
-
 export default app;
